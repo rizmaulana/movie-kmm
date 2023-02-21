bf75df3b86183e3ea8e6da2ad1723e003fa0dbc5
Rizki <almaulanarizki@gmail.com>
2023-02-21 15:41:50 +0700

    feat: add cocoapods

---
+++ iosApp/iosApp/Screen/MovieScreen.swift
@@ -0,0 +1,21 @@
+//
+//  MovieScreen.swift
+//  iosApp
+//
+//  Created by Rizki Maulana on 21/02/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct MovieScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieScreen()
+    }
+}
